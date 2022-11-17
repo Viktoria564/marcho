549fa3a54708654972ce7f62d9d03d1d2949423f
Viktoria564 <neciporenkoviktoria7@gmail.com>
2022-11-17 17:53:18 +0200

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,33 @@
 $(function () {
 
+  $('.shop-content__filter-button').on('click', function () {
+    $('.shop-content__filter-button').removeClass('shop-content__filter-button--active');
+      $(this).addClass('shop-content__filter-button--active');
+  });  
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  })
+
+  $('.select-style').styler();
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
